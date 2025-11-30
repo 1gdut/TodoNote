4cb899f08b587c74522a7d85338378c6da948f01
xiaoruitao <1226324785@qq.com>
2025-11-30 16:13:17 +0800

    feat: 添加侧边栏控制器及其功能，支持菜单项选择和关闭操作

--- TodoNote/viewcontroller/MainViewController.swift
+++ TodoNote/viewcontroller/MainViewController.swift
@@ -6,8 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    /// 侧边栏是否显示
+    private var isSideMenuOpen = false
+    
+    /// 侧边栏宽度
+    private let sideMenuWidth: CGFloat = 280
+    
+    // MARK: - UI Components
+    
+    /// 遮罩层
+    private lazy var dimmingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        view.alpha = 0
+        view.isUserInteractionEnabled = false
+        return view
+    }()
+    
+    /// 侧边栏容器
+    private lazy var sideMenuContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.3
+        view.layer.shadowOffset = CGSize(width: 2, height: 0)
+        view.layer.shadowRadius = 5
+        return view
+    }()
+    
+    /// 侧边栏控制器
+    private lazy var sideMenuVC: SideMenuViewController = {
+        let vc = SideMenuViewController()
+        vc.delegate = self
+        return vc
+    }()
 
     // MARK: - Lifecycle
     
@@ -15,6 +53,11 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupNavigationBar()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupSideMenu()
     }
     
     // MARK: - Setup
@@ -54,6 +97,39 @@ class MainViewController: UIViewController {
         )
     }
     
+    private var sideMenuSetup = false
+    
+    private func setupSideMenu() {
+        guard !sideMenuSetup else { return }
+        sideMenuSetup = true
+        
+        guard let window = view.window else { return }
+        
+        // 添加到 window 上
+        window.addSubview(dimmingView)
+        window.addSubview(sideMenuContainer)
+        
+        // 设置遮罩层 frame
+        dimmingView.frame = window.bounds
+        
+        // 设置侧边栏 frame，初始在屏幕左侧外
+        sideMenuContainer.frame = CGRect(
+            x: -sideMenuWidth,
+            y: 0,
+            width: sideMenuWidth,
+            height: window.bounds.height
+        )
+        
+        // 直接添加侧边栏视图（不使用 child view controller）
+        sideMenuVC.delegate = self
+        sideMenuContainer.addSubview(sideMenuVC.view)
+        sideMenuVC.view.frame = sideMenuContainer.bounds
+        
+        // 遮罩层点击手势
+        let tap = UITapGestureRecognizer(target: self, action: #selector(closeSideMenu))
+        dimmingView.addGestureRecognizer(tap)
+    }
+    
     // MARK: - Actions
     
     @objc private func aiButtonTapped() {
@@ -62,7 +138,64 @@ class MainViewController: UIViewController {
     }
     
     @objc private func menuButtonTapped() {
-        print("点击了菜单按钮")
-        // TODO: 打开侧边菜单
+        if isSideMenuOpen {
+            closeSideMenu()
+        } else {
+            openSideMenu()
+        }
+    }
+    
+    // MARK: - Side Menu Control
+    
+    private func openSideMenu() {
+        isSideMenuOpen = true
+        dimmingView.isUserInteractionEnabled = true
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
+            self.sideMenuContainer.frame.origin.x = 0
+            self.dimmingView.alpha = 1
+        }
+    }
+    
+    @objc private func closeSideMenu() {
+        isSideMenuOpen = false
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
+            self.sideMenuContainer.frame.origin.x = -self.sideMenuWidth
+            self.dimmingView.alpha = 0
+        } completion: { _ in
+            self.dimmingView.isUserInteractionEnabled = false
+        }
+    }
+}
+
+// MARK: - SideMenuDelegate
+
+extension MainViewController: SideMenuDelegate {
+    
+    func sideMenuDidSelectItem(_ item: SideMenuItem) {
+        closeSideMenu()
+        
+        switch item {
+        case .profile:
+            print("打开个人资料")
+            // TODO: 跳转到个人资料页面
+            
+        case .settings:
+            print("打开设置")
+            // TODO: 跳转到设置页面
+            
+        case .about:
+            print("打开关于")
+            // TODO: 跳转到关于页面
+            
+        case .logout:
+            print("退出登录")
+            // TODO: 处理退出登录逻辑
+        }
+    }
+    
+    func sideMenuDidClose() {
+        closeSideMenu()
     }
 }
